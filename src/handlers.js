d4a61a3c0138eca5f2eefa12df1754ff15a22090
JRoeseph <48298846+JRoeseph@users.noreply.github.com>
2021-10-25 01:06:48 -0400

    Added ping and removed dice

--- src/handlers.js
+++ src/handlers.js
@@ -10,19 +10,18 @@ const onMessageHandler = (client, target, context, msg, self) => {
     // We use two regexes here because if there is a period we only want to go to the end of the sentence
     let imJoke = msg.match(/(^| )[iI]'?[mM][\w ]*\./);
     if (imJoke) {
-      lib.imJoke(client, imJoke, target);
+      lib.imJoke(client, imJoke, target, context.username);
     } else {
       imJoke = msg.match(/(^| )[iI]'?[mM][\w ]*/);
-      if (imJoke) lib.imJoke(client, imJoke, target);
+      if (imJoke) lib.imJoke(client, imJoke, target, context.username);
     }
 
     // Obviously, if it's not a command we don't care
     if (!msg.startsWith('!')) return;
     const commandName = msg.trim();
   
-    if (commandName === '!dice') {
-      const num = lib.rollDice();
-      client.say(target, `You rolled a ${num}`);
+    if (commandName === '!ping') {
+      client.say(target, 'Pong!');
       console.log(`* Executed ${commandName} command`);
     } else {
       console.log(`* Unknown command ${commandName}`);
--- src/lib.js
+++ src/lib.js
@@ -1,14 +1,9 @@
-const rollDice = () => {
-    const sides = 6;
-    return Math.floor(Math.random() * sides) + 1;
-}
-
-const imJoke = (client, message, target) => {
+const imJoke = (client, message, target, sender) => {
     const name = message[0].substring(message[0].indexOf(' ')+1);
     client.say(target, `Hi "${name}", I'm BeepBoBoopBot!`);
+    console.log(`* ${sender} just got dad joked`)
 }
 
 module.exports = {
-    rollDice,
     imJoke,
 }
