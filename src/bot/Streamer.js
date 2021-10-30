b6eecdb3c6ffe96cf2f105e438e91931ff261a92
JRoeseph <48298846+JRoeseph@users.noreply.github.com>
2021-10-29 20:37:54 -0400

    Fixed !level again

--- src/bot/Streamer.js
+++ src/bot/Streamer.js
@@ -63,7 +63,7 @@ class Streamer {
   }
 
   async getUserLevel(username) {
-    const userDoc = await this.userInfo.findOne({username}).lean();
+    const userDoc = await this.userInfo.findOne({username: username.toLowerCase()}).lean();
     const rank = (await this.userInfo.countDocuments({xp: {$gt: userDoc.xp}}))+1;
     const requiredXP = 16*(userDoc.level+1)*(userDoc.level+1)+100*(userDoc.level+1)-16;
     return {xp: userDoc.xp, requiredXP, level: userDoc.level, rank};
